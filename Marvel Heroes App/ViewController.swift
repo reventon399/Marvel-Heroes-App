47509026c4c497f8535dba7825bb8f5eb43fc575
Alexey <alexeylosev1501@gmail.com>
2022-11-11 19:08:55 +0300

    Initial Commit

---
+++ Marvel Heroes App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel Heroes App
+//
+//  Created by Алексей Лосев on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
