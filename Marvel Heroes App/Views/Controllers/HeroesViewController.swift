a3fe9b4523b1bf20d092b125dbd7fa50b23488a4
Alexey <alexeylosev1501@gmail.com>
2022-11-14 21:27:45 +0300

    added model structs.

--- Marvel Heroes App/Views/Controllers/HeroesViewController.swift
+++ Marvel Heroes App/Views/Controllers/HeroesViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class HeroesViewController: UIViewController {
 
+    let publicKey = "e1198af2a04c8e71fbe11a5b2f5de361"
+    let privateKey = "61e5bc3ae975a2d192a9512ad456d7e16c41757f"
+    let timestamp = Date().timeIntervalSince1970.description
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        fetchRequest()
     }
 
-
+    func fetchRequest() {
+        let request =  "https://gateway.marvel.com/v1/public/characters?ts=\(timestamp)&apikey=\(publicKey)&hash=\(hash)"
+    }
 }
 
