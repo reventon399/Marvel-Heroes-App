e4e9eaed5bfa04da575e73e311bff0f825880fb7
Alexey <alexeylosev1501@gmail.com>
2022-11-15 22:37:10 +0300

    model changes.

---
+++ Marvel Heroes App/Extensions/String + MD5.swift
@@ -0,0 +1,20 @@
+//
+//  String + MD5.swift
+//  Marvel Heroes App
+//
+//  Created by Алексей Лосев on 14.11.2022.
+//
+
+import Foundation
+import CryptoKit
+
+extension String {
+    var MD5: String {
+        let computed = Insecure.MD5.hash(data: self.data(using: .utf8)!)
+        return computed.map { String(format: "%02hhx", $0) }.joined()
+    }
+    
+    static func getUrlString(image: Image, variant: String) -> String {
+        return "\(image.path)/\(variant).\(image.extension)"
+    }
+}
---
+++ Marvel Heroes App/ViewModels/Parser.swift
@@ -0,0 +1,14 @@
+//
+//  Parser.swift
+//  Marvel Heroes App
+//
+//  Created by Алексей Лосев on 14.11.2022.
+//
+
+import Foundation
+
+class URLConfiguration {
+    func getUrl() {
+        
+    }
+}
