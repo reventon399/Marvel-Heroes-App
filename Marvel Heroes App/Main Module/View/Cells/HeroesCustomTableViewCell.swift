e4fc3a8396df2a55768e3889212212eb49e0278f
Alexey <alexeylosev1501@gmail.com>
2022-11-23 15:09:50 +0300

    setup layout

--- Marvel Heroes App/Main Module/View/Cells/HeroesCustomTableViewCell.swift
+++ Marvel Heroes App/Main Module/View/Cells/HeroesCustomTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class HeroesCustomTableViewCell: UITableViewCell {
 
@@ -13,14 +14,14 @@ class HeroesCustomTableViewCell: UITableViewCell {
     
     // MARK: - Outlets
     
-    private lazy var heroImageView: UIImageView = {
-       let imageView = UIImageView()
+    private lazy var heroImageView: UIView = {
+       let imageView = UIView()
         
         return imageView
     }()
     
-    private lazy var heroImage: UIImage = {
-       let image = UIImage()
+    private lazy var heroImage: UIImageView = {
+       let image = UIImageView()
         
         return image
     }()
@@ -52,13 +53,40 @@ class HeroesCustomTableViewCell: UITableViewCell {
     // MARK: - Setup
     
     private func setupHierarchy() {
-        
+        addSubview(heroNameLabel)
+        addSubview(heroDescriptionLabel)
+        addSubview(heroImageView)
+        addSubview(heroImage)
     }
     
     private func setupLayout() {
+        heroImageView.snp.makeConstraints { make in
+            make.centerY.equalTo(self)
+            make.left.equalTo(self).offset(20)
+            make.height.width.equalTo(60)
+        }
         
+        heroImage.snp.makeConstraints { make in
+            make.center.equalTo(heroImageView)
+            make.height.width.equalTo(60)
+        }
+        
+        heroNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(self).offset(10)
+            make.left.equalTo(heroImageView.snp.right).offset(20)
+            make.right.equalTo(self).offset(-20)
+        }
+        
+        heroDescriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(heroNameLabel.snp.bottom).offset(10)
+            make.left.equalTo(heroImageView.snp.right).offset(20)
+            make.right.equalTo(self).offset(-20)
+        }
     }
     
     // MARK: - Reuse
-
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.accessoryType = .none
+    }
 }
